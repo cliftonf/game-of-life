e7d011206f68889d063b0d3562445fea4f52cdff
cliftonf <clifton.fischlein@gmail.com>
2017-06-13 10:56:40 -0400

    Create Cell.java

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -1,4 +1,4 @@
-package com.wakaleo.gameoflife.domain;
++ackage com.wakaleo.gameoflife.domain;
 
  /**
  * Wakaleo Consulting - John Ferguson Smart
@@ -9,7 +9,7 @@ package com.wakaleo.gameoflife.domain;
  */
 public enum Cell {
 	// Symbols to represent cell status
-    LIVE_CELL("*"), DEAD_CELL(".");
+    LIVE_CELL("+"), DEAD_CELL(".");
 
     private String symbol;
 
